7ad7c4d6edcc5fd29525bd8e747be2bbe7b5b5d3
Jmetrics86 <jsnbrwstr@gmail.com>
2018-11-17 13:51:28 -0500

    Committed the Staged Script Changes

--- myscript.R
+++ myscript.R
@@ -1,2 +1,7 @@
 # My new script for learning R
 x = 1:10
+
+#Add in the packages
+x <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap")
+# install.packages(x) # warning: uncommenting this may take a number of minutes
+lapply(x, library, character.only = TRUE) # load the required packages
